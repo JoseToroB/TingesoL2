e2d83126250c2451fdbee0df551f4ff798f3cbc8
JoseToroB <jose.toro.b@usach.cl>
2023-11-08 19:55:43 -0300

    config data

--- prueba-service/src/main/java/com/example/pruebaservice/controllers/PruebaController.java
+++ prueba-service/src/main/java/com/example/pruebaservice/controllers/PruebaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
-@RequestMapping
+@RequestMapping("/pruebas")
 public class PruebaController {
     @Autowired
     PruebaService pruebaService;
